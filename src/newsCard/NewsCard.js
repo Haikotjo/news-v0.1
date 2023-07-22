2d29dd3d2242813a3c0807a99eac62544ec9170a
Haikotjo <haikowierda@hotmail.com>
2023-07-22 17:38:27 +0200

    ik wet eigenlijk niet zo goed wat ik heb gedaan

--- src/newsCard/NewsCard.js
+++ src/newsCard/NewsCard.js
@@ -1,23 +1,35 @@
-
 import React, { useState } from 'react';
-import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 const NewsCard = ({ article }) => {
     const [isExpanded, setIsExpanded] = useState(false);
 
-    const handleClick = () => {
-        setIsExpanded(!isExpanded);
+    const shortenText = (text, numberOfWords, isExpanded) => {
+        const words = text.split(' ');
+        if (words.length > numberOfWords && !isExpanded) {
+            return words.slice(0, numberOfWords).join(' ') + ' ...';
+        }
+        return text;
     };
 
     return (
-        <Card style={{ maxWidth: '18rem', height: isExpanded ? 'auto' : '300px', overflow: 'hidden', margin: '1rem'  }}>
+        <Card style={{ maxWidth: '18rem', height: 'auto', overflow: 'hidden', margin: '1rem'  }}>
             {article.urlToImage && <Card.Img variant="top" src={article.urlToImage} />}
             <Card.Body>
-                <Card.Title>{article.title}</Card.Title>
-                {article.description && <Card.Text>{article.description}</Card.Text>}
-                <Button variant="primary" href={article.url}>Read more</Button>
-                <Button variant="secondary" onClick={handleClick}>{isExpanded ? 'Show less' : 'Show more'}</Button>
+                <Card.Title>
+                    {shortenText(article.title, 5, isExpanded)}
+                    {article.title.split(' ').length > 5 &&
+                        <p href="#" onClick={() => setIsExpanded(!isExpanded)} style={{color: 'blue', cursor: 'pointer'}}>
+                            {isExpanded ? ' Show less' : ' Show more'}
+                        </p>
+                    }
+                </Card.Title>
+                {isExpanded && article.description &&
+                    <Card.Text>
+                        {article.description}
+                    </Card.Text>}
+                <Button variant="primary" href={article.url}>Source</Button>
             </Card.Body>
         </Card>
     );
